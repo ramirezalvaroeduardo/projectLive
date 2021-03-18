561648334c4cb0ac312cf928b0de4bcead55ba95
eduardo.ramirez <eduardo.ramirez@crossmatch.com>
2021-03-17 23:34:38 -0400

    ER - Modev plserver to root.

---
+++ server.js
@@ -0,0 +1,66 @@
+
+var express		= require('express');
+var bodyParser	= require('body-parser');
+var path		= require('path');
+var sqlite3		= require('sqlite3');
+var db			= new sqlite3.Database('projectlife.db');
+var app			= express();
+var port		= 3010;
+
+				
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(express.static(path.join(__dirname, './plclient/build')));
+
+var lisApp = function() {
+	console.log('App listening on port [' + port + ']');
+}
+
+var mainPage = function(req,res){
+	res.sendFile(path.join(__dirname, './plclient/build/index.html'));
+}
+
+var getAllParticipants = function(req, res){
+	db.all('SELECT rowid, * FROM projectlife', function(err, rows){
+		if(err){
+			res.send(err.message);
+		}else{
+			console.log('Response: Sent payload.');
+			res.json(rows);
+		}
+	});
+}
+
+var addNewParticipant  = function(req, res){
+	let newPart = JSON.parse(
+		JSON.stringify(req.body)
+		.replace(/\{\"/, '' )
+		.replace(/\":\"\"\}/, '')
+		.replace(/\\/g, '')
+	).newPart;
+	db.run('INSERT INTO projectlife VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+		[
+			newPart.FullName,
+			newPart.DOB,
+			newPart.Age,
+			newPart.Gender,
+			newPart.Ethnicity,
+			newPart.Phone,
+			newPart.Email,
+			newPart.Address1,
+			newPart.Address2,
+			newPart.City,
+			newPart.State,
+			newPart.Zip,
+			newPart.County,
+			newPart.HasTransport,
+		]
+	);
+	res.send({'Server_Response':'Arrived to addNewParticipant'});
+}
+
+app.get('/', mainPage);
+app.get('/getAllParticipants', getAllParticipants);
+app.post('/addNewParticipant', addNewParticipant);
+
+app.listen( port, lisApp);
+
